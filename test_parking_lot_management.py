de9aac70d3b1059b9a3c3e848bc2f09f44f5b989
Akshaya <akshayachandorkar29@gmail.com>
2020-03-02 18:15:49 +0530

    Added: test case for checking the slot status

--- test_parking_lot_management.py
+++ test_parking_lot_management.py
@@ -17,7 +17,7 @@ def test_get_type_of_vehicle():
 
 # test case for checking created parking slot
 def test_create_parking_lot():
-    assert parking.create_parking_lot(100) == 100
+    assert parking.create_parking_lot(2) == 2
 
 
 # test case to getting empty slot when all the slots are empty
@@ -30,8 +30,13 @@ def test_park_the_car():
     assert parking.park(101, "red") == 1
 
 
+# test case for unparking
 def test_leave_the_car():
     assert parking.leave(1) == True
 
+
+# test case for getting status
+def test_get_status():
+    assert parking.get_status() == "VACANT"
 
 
