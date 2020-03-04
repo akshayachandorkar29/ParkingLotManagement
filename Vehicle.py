2875414984ab73d5a40735df9a556a4d55d183ea
Akshaya <akshayachandorkar29@gmail.com>
2020-03-04 18:04:47 +0530

    Refactored: added name attribute

--- Vehicle.py
+++ Vehicle.py
@@ -5,7 +5,8 @@ Date: 02/03/2020
 
 # this is parent class for every vehicle type
 class Vehicle:
-    def __init__(self, reg_no, color, current_time):
+    def __init__(self, name, reg_no, color, current_time):
+        self.name = name
         self.color = color
         self.reg_no = reg_no
         self.current_time = current_time
@@ -13,8 +14,8 @@ class Vehicle:
 
 # this class has extended parent class Vehicle
 class Car(Vehicle):
-    def __init__(self, reg_no, color, current_time):
-        Vehicle.__init__(self, reg_no, color, current_time)
+    def __init__(self, name, reg_no, color, current_time):
+        Vehicle.__init__(self, name, reg_no, color, current_time)
 
     def get_type(self):  # this will return the type of the vehicle
         return "Car"
