e9ce90409cc9702b826f95bba207b28a1032bb40
Akshaya <akshayachandorkar29@gmail.com>
2020-03-04 12:49:05 +0530

    Added: function to search the car or slot or time given color, reg_no, time

--- parking_lot_management.py
+++ parking_lot_management.py
@@ -4,6 +4,9 @@ Author: Akshaya Revaskar
 Date: 02/01/2020
 """
 import Vehicle
+import datetime
+now = datetime.datetime.now()
+curr_time = datetime.time(now.hour, now.minute, now.second)
 
 
 # this is a main driver class which have all the business logic for efficient parking lot management
@@ -12,6 +15,7 @@ class ParkingLot:
         self.capacity = 0
         self.slot_id = 0
         self.num_of_occupied_slots = 0
+
 
     # this function will create an empty parking slot with defined capacity
     def create_parking_lot(self, capacity):
@@ -26,10 +30,10 @@ class ParkingLot:
                 return i
 
     # this is the function to actually park the Car
-    def park(self, reg_no, color):
+    def park(self, reg_no, color, current_time):
         if self.num_of_occupied_slots < self.capacity:
             slot_id = self.get_empty_slot()
-            self.slots[slot_id] = Vehicle.Car(reg_no, color)
+            self.slots[slot_id] = Vehicle.Car(reg_no, color, current_time)
             self.slot_id = self.slot_id + 1
             self.num_of_occupied_slots = self.num_of_occupied_slots + 1
             return slot_id + 1
@@ -61,3 +65,82 @@ class ParkingLot:
         else:
             redirect = False
         return redirect
+
+    # this is getter function for registration number given color
+    def get_reg_no_from_color(self, color):
+
+        reg_nos = []
+        for i in self.slots:
+
+            if i == -1:
+                continue
+            if i.color == color:
+                reg_nos.append(i.reg_no)  # append registration number to the list reg_no
+        return reg_nos
+
+    # this is getter function for slot number given registration number
+    def get_slot_no_from_reg_no(self, reg_no):
+
+        for i in range(len(self.slots)):  # traverse through slot list and return slot number
+            if self.slots[i].reg_no == reg_no:
+                return i + 1
+            else:
+                continue
+        return -1
+
+    # this is getter function for slot number given color
+    def get_slot_no_from_color(self, color):
+
+        slot_nos = []
+
+        for i in range(len(self.slots)):
+
+            if self.slots[i] == -1:
+                continue
+            if self.slots[i].color == color:
+                slot_nos.append(str(i + 1))
+        return slot_nos
+
+    # this function is for helping driver to find his car given color of the car
+    def search_car_given_color(self, color):
+        return self.get_slot_no_from_color(color)
+
+    # this function is for helping driver to find his car given registration number of the car
+    def search_car_given_reg_no(self, reg_no):
+        slot_of_the_car = self.get_slot_no_from_reg_no(reg_no)
+        return slot_of_the_car
+
+    # this function returns reg no of all the cars given time
+    def get_reg_no_from_time(self, current_time):
+
+        reg_no = []
+        for i in self.slots:
+
+            if i == -1:
+                continue
+            if i.current_time == current_time:
+                reg_no.append(i.reg_no)  # append registration number to the list reg_no
+        return reg_no
+
+    # this function returns reg no of all the cars given time
+    def get_time_from_reg_no(self, reg_no):
+
+        time = []
+        for i in self.slots:
+
+            if i == -1:
+                continue
+            if i.reg_no == reg_no:
+                time.append(i.current_time)  # append registration number to the list reg_no
+        return time
+
+
+a = ParkingLot()
+a.create_parking_lot(5)
+a.park(101, "white", curr_time.hour)
+a.park(102, "red", curr_time.hour)
+a.park(103, "white", curr_time.hour)
+b = a.search_car_given_color("white")
+print(b)
+
+
