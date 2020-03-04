976185d1de1bb66fc6c8a859f6bc9f74856a7feb
Akshaya <akshayachandorkar29@gmail.com>
2020-03-04 12:47:58 +0530

    Refactored: Added current time while parking the car

--- Vehicle.py
+++ Vehicle.py
@@ -5,15 +5,17 @@ Date: 02/03/2020
 
 # this is parent class for every vehicle type
 class Vehicle:
-    def __init__(self, reg_no, color):
+    def __init__(self, reg_no, color, current_time):
         self.color = color
         self.reg_no = reg_no
+        self.current_time = current_time
 
 
 # this class has extended parent class Vehicle
 class Car(Vehicle):
-    def __init__(self, reg_no, color):
-        Vehicle.__init__(self, reg_no, color)
+    def __init__(self, reg_no, color, current_time):
+        Vehicle.__init__(self, reg_no, color, current_time)
 
     def get_type(self):  # this will return the type of the vehicle
         return "Car"
+
